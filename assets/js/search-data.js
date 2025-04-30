78aba6477fa29ebfc92a546145f6829c0c7f8921
sunilsarolkarcds <131423506+sunilsarolkarcds@users.noreply.github.com>
2025-04-30 05:37:30 +0000

    Deploying to gh-pages from @ spsarolkar/spsarolkar.github.io@b2788a9544048bc5c51249c7baa4d87def43abd0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Seeking a data engineering/science role focused on big data, Spark, and intelligent pipelines—blending my software expertise with a passion for scalable analytics and continuous learning.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -193,6 +193,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_1/";
+            },},{id: "news-",
+          title: '',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
             },},{id: "projects-structformer",
           title: 'StructFormer',
           description: "Transformer-based model built to automate structured data transformation from one schema to another",
